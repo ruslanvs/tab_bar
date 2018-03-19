8b8596d52f76b1426177f458b1c79f46224e1bd2
ruslanvs <ruslanvs@yahoo.com>
2018-03-19 14:48:33 -0700

    working, but seems reversed

---
+++ A.swift
@@ -0,0 +1,38 @@
+//
+//  A.swift
+//  tab_bar
+//
+//  Created by Ruslan Suvorov on 3/19/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import UIKit
+
+class A: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("A viewDidLoad")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("A viewWillAppear")
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
